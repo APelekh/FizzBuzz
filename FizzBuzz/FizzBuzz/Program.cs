8c4da3e57eb6ec9e5fc60f10edbf8a1596123bc5
Andrii Pelekh <pelekhav@gmail.com>
2015-02-02 15:16:39 -0700

    FizzBuzzDone

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -14,7 +14,43 @@ namespace FizzBuzz
         }
         public static string FizzBuzz(int number)
         {
-            return "";
+            if (number % 3 == 0 && number % 5 == 0 ){
+
+                return "FizzBuzz";
+            }
+            
+            else if (number % 3 == 0){
+
+                return "Buzz";
+            }
+
+            else if (number % 5 == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+                return Convert.ToString(number);
+            }
+
+        }
+
+        public static string Buzz(int number)
+        {
+            if (number % 3 == 0){
+                return "Buzz";
+            }
+            return Convert.ToString(number);
+            
+        }
+
+        public static string Fizz(int number)
+        {
+            if (number % 5 == 0)
+            {
+                return "Fizz";
+            }
+            return Convert.ToString(number);
         }
     }
 }
